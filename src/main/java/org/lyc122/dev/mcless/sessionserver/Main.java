62c3276d31f982ee0452230460955191111d8782
Li Yichen <Ericlyc122@outlook.com>
2025-12-15 21:41:56 +0800

    Innitial Commit

---
+++ src/main/java/org/lyc122/dev/mcless/sessionserver/Main.java
@@ -0,0 +1,9 @@
+package org.lyc122.dev.mcless.sessionserver;
+
+public class Main {
+    public static String versionId = "v0.1";
+    public static void main(String[] args) {
+        System.out.printf("Starting LESS Session Server "+versionId);
+
+    }
+}
